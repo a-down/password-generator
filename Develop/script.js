54af7c4a24e528043fc5ff212b8e38dd952cc970
a-down <136649391+a2downing@users.noreply.github.com>
2023-06-30 13:25:31 -0500

    defined pass attributes and put functions under generatePassword()

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+var passLength;
+var passLowercase;
+var passUppercase;
+var passNumbers;
+var passSpecial;
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(){
+  generateLength();
+  generateLowercase();
+  generateUppercase();
+  generateNumbers();
+  generateSpecialCharacters();
+}
 
 //  length of password (8-128)
 function generateLength() {
-  var passLength = prompt("How many characters long should the password be? (between 8-128 characters)")
+  passLength = prompt("How many characters long should the password be? (between 8-128 characters)")
   if (passLength < 8) {
     alert("Please choose a longer password length.");
     generateLength();
@@ -18,7 +43,7 @@ function generateLength() {
 
 //  lowercase?
 function generateLowercase() {
-  var passLowercase = prompt("Should the password have lowercase letters? (yes or no)"); 
+  passLowercase = prompt("Should the password have lowercase letters? (yes or no)"); 
   if (passLowercase !== "yes" && passLowercase !== "no") {
     alert("Please try again. Answer yes or no.");
     generateLowercase();
@@ -29,7 +54,7 @@ function generateLowercase() {
 
 //  uppercase?
 function generateUppercase() {
-  var passUppercase = prompt("Should the password have uppercase letters? (yes or no)");
+  passUppercase = prompt("Should the password have uppercase letters? (yes or no)");
   if (passUppercase !== "yes" && passUppercase !== "no") {
     alert("Please try again. Answer yes or no.");
     generateUppercase();
@@ -40,7 +65,7 @@ function generateUppercase() {
 
 //  numbers?
 function generateNumbers() {
-  var passNumbers = prompt("Should the password have numbers? (yes or no)");
+  passNumbers = prompt("Should the password have numbers? (yes or no)");
   if (passNumbers !== "yes" && passNumbers !== "no") {
     alert("Please try again. Answer yes or no.");
     generateNumbers();
@@ -51,7 +76,7 @@ function generateNumbers() {
 
 //  special characters?
 function generateSpecialCharacters() {
-  var passSpecial = prompt("Should the password have special characters? (yes or no)");
+  passSpecial = prompt("Should the password have special characters? (yes or no)");
   if (passSpecial !== "yes" && passSpecial !== "no") {
     alert("Please try again. Answer yes or no.");
     generateSpecialCharacters();
@@ -59,56 +84,3 @@ function generateSpecialCharacters() {
     return;
   }
 }
-
-/*
-//  get user attributes
-var generateAttributes = function() {
-  generateLength();
-  generateLowercase();
-  generateUppercase();
-  generateNumbers();
-  generateSpecialCharacters();
-  return
-}
-*/
-
-
-//  define variables for the userAttributes Object
-var userLength
-var userLowercase
-var userUppercase
-var userNumbers
-var userSpecial
-
-
-function generatePassword() {
-  userLength = generateLength();
-  userLowercase = generateLowercase();
-  userUppercase = generateUppercase();
-  userNumbers = generateNumbers();
-  userSpecial = generateSpecialCharacters();
-}
-
-var userAttributes = {
-  length: userLength,
-  lowercase: userLowercase,
-  uppercase: userUppercase,
-  numbers: userNumbers,
-  special: userSpecial,
-}
-
-generatePassword();
-console.log(userAttributes);
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
